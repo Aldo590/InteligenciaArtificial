5d0bf9561282a70394724878eec89748ab65676d
Aldo Rubio <a22310381@ceti.mx>
2026-02-27 20:48:56 -0600

    Modulo 2

---
+++ Curso1/ProgramaImprimir.py
@@ -0,0 +1,51 @@
+#2.1.1 Tu primer programa
+print("¡Hola, Mundo!")
+
+#2.1.7 Instrucciones
+print("La Witsi Witsi Araña subió a su telaraña.")
+print("Vino la lluvia y se la llevó.")
+
+print("La Witsi Witsi Araña subió a su telaraña.")
+print()
+print("Vino la lluvia y se la llevó.")
+
+#2.1.8 Caracteres de escape y nueva línea en Python
+print("La Witsi Witsi Araña\nsubió a su telaraña.")
+print()
+print("Vino la lluvia\ny se la llevó.")
+
+#2.1.9 Usando múltiples argumentos
+print("La Witsi Witsi Araña" , "subió" , "a su telaraña.")
+
+#2.1.10 Argumentos posicionales
+print("Mi nombre es", "Python.")
+print("Monty Python.")
+
+#2.1.11 Argumentos de palabra clave
+print("Mi nombre es ", end="...")
+print("Monty Python.")
+
+print("Mi", "nombre", "es", "Monty", "Python.", sep="-")
+
+print("Mi", "nombre", "es", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
+#2.1.12 La función print() y sus argumentos
+
+print("Programming","Essentials","in",sep='***',end='...')
+print("Python")
+
+#2.1.13 Dando formato a la salida
+
+
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****") #Minimizar Prints
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****\n"*2) #Doble flecha
